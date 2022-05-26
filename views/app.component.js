5da29b0cdd04c851ae0215c7d767d3d4d6d3c408
Sofia Vega <sofia.vega18@gmail.com>
2022-05-26 16:48:06 -0500

    ahora si jala los tickets de la base de datos

--- views/app.component.js
+++ views/app.component.js
@@ -11,12 +11,14 @@ var app = Vue.createApp({
         grouped_tickets() {
             console.log("hola")
             gt = [];
-            tickets = this.tickets_2
+            this.tickets_2
             const chunkSize = 3;
             for (let i = 0; i < this.tickets.length; i += chunkSize) {
                 const chunk = this.tickets.slice(i, i + chunkSize);
                 gt.push(chunk);
             }
+            console.log("grouped tickets")
+            console.log(gt)
             return gt;
           // returns a nested array: 
           // [[article, article, article], [article, article, article], ...]
@@ -25,15 +27,14 @@ var app = Vue.createApp({
             //this.tickets = tickets.getAll()
             console.log("ticket2")
             var t_data
-            var url = 'http://localhost:3000/tickets/getAll/'
+            var url = 'http://localhost:3000/tickets/'
 
             fetch(url)
             .then(res => res.json())
             .catch(error => console.error('Error:', error))
-            .then(data => t_data = data).then(data => this.tickets = data).then(data => console.log(t_data))
+            .then(data => t_data = data).then(data => this.tickets = data.data).then(data => console.log(t_data.data))
 
             //this.tickets = t_data
-            console.log(t_data)
 
             return t_data
         }
