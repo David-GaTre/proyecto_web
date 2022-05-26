12b0054ac61a41931be6608830e4aef022216f51
Sofia Vega <sofia.vega18@gmail.com>
2022-05-26 17:09:20 -0500

    cambiar a ruta tickets/uncompleted

--- views/app.component.js
+++ views/app.component.js
@@ -1,10 +1,7 @@
-//import express from 'express';
-//import tickets from '../services/tickets'
 var app = Vue.createApp({
     data() {
         return {
             tickets: []
-            //tickets: [{titulo: "uno"}, {"titulo": "dos"}, {titulo: "tres"}, {"titulo": "cuatro"}]
         }
     },
     computed:{
@@ -17,41 +14,18 @@ var app = Vue.createApp({
                 const chunk = this.tickets.slice(i, i + chunkSize);
                 gt.push(chunk);
             }
-            console.log("grouped tickets")
-            console.log(gt)
             return gt;
-          // returns a nested array: 
-          // [[article, article, article], [article, article, article], ...]
         },
         tickets_2(){
-            //this.tickets = tickets.getAll()
-            console.log("ticket2")
             var t_data
-            var url = 'http://localhost:3000/tickets/'
+            var url = 'http://localhost:3000/tickets/uncompleted'
 
             fetch(url)
             .then(res => res.json())
             .catch(error => console.error('Error:', error))
             .then(data => t_data = data).then(data => this.tickets = data.data).then(data => console.log(t_data.data))
 
-            //this.tickets = t_data
-
             return t_data
         }
     }
-    /*
-    methods: {
-        handleLogin() {
-            var u_data
-            var url = 'http://localhost:3000/users/user_check/' + this.user + '&' + this.pass;
-
-            fetch(url)
-            .then(res => res.json())
-            .catch(error => console.error('Error:', error))
-            .then(data => u_data = data)
-            .then(() => document.cookie = "user_id=" + u_data.data.id)
-            .then(() => window.location.replace("http://localhost:3000/profile"))
-        },
-    },
-    */
 })
