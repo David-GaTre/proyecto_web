0fa89eb8a0db40848f730bc823c7eae6b6dd8ef7
Sofia Vega <sofia.vega18@gmail.com>
2022-05-26 16:38:22 -0500

    creo que ya toma los tickets de sqlite

--- views/app.component.js
+++ views/app.component.js
@@ -1,10 +1,56 @@
+//import express from 'express';
+//import tickets from '../services/tickets'
 var app = Vue.createApp({
     data() {
         return {
-            grouped_tickets: [
-                [{titulo: "uno"}, {"titulo": "dos"}],
-                [{titulo: "tres"}, {"titulo": "cuatro"}]
-            ]
+            tickets: []
+            //tickets: [{titulo: "uno"}, {"titulo": "dos"}, {titulo: "tres"}, {"titulo": "cuatro"}]
+        }
+    },
+    computed:{
+        grouped_tickets() {
+            console.log("hola")
+            gt = [];
+            tickets = this.tickets_2
+            const chunkSize = 3;
+            for (let i = 0; i < this.tickets.length; i += chunkSize) {
+                const chunk = this.tickets.slice(i, i + chunkSize);
+                gt.push(chunk);
+            }
+            return gt;
+          // returns a nested array: 
+          // [[article, article, article], [article, article, article], ...]
+        },
+        tickets_2(){
+            //this.tickets = tickets.getAll()
+            console.log("ticket2")
+            var t_data
+            var url = 'http://localhost:3000/tickets/getAll/'
+
+            fetch(url)
+            .then(res => res.json())
+            .catch(error => console.error('Error:', error))
+            .then(data => t_data = data).then(data => this.tickets = data).then(data => console.log(t_data))
+
+            //this.tickets = t_data
+            console.log(t_data)
+
+            return t_data
         }
     }
+    /*
+    methods: {
+        handleLogin() {
+            var u_data
+            var url = 'http://localhost:3000/users/user_check/' + this.user + '&' + this.pass;
+
+            fetch(url)
+            .then(res => res.json())
+            .catch(error => console.error('Error:', error))
+            .then(data => u_data = data)
+            .then(() => document.cookie = "user_id=" + u_data.data.id)
+            .then(() => window.location.replace("http://localhost:3000/profile"))
+        },
+    },
+    */
 })
