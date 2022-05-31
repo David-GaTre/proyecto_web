437d0689ec1ea9e279d14950d03fef60f174c85a
David Garcia <gtdavid2013@gmail.com>
2022-05-31 18:03:02 -0500

    Corrected cookie obtainance

--- views/history.component.js
+++ views/history.component.js
@@ -15,10 +15,26 @@ var history = Vue.createApp({
             }
             return gt;
         },
+        getCookie(cname) {
+            let name = cname + "=";
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let ca = decodedCookie.split(';');
+            for(let i = 0; i <ca.length; i++) {
+              let c = ca[i];
+              while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+              }
+              if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+              }
+            }
+            return "";
+          },
+
         tickets_2(){
             var t_data
-            var url = 'http://localhost:3000/tickets//history/:user_id'
-
+            var url = 'http://localhost:3000/tickets/history/' + this.getCookie(user_id)
+            console.log(url)
             fetch(url)
             .then(res => res.json())
             .catch(error => console.error('Error:', error))
