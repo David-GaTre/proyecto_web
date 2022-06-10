89dc85e157d6f6a75b8140b93e1a2efab708ed5c
A01197185 <A01197185@itesm.mx>
2022-06-10 11:44:57 -0500

    Creación Branch
    Se creo branch y se comenzo con el trabajo de las cards y el ticketview

---
+++ views/sidebar.component.js
@@ -0,0 +1,41 @@
+app.component('sidebar', {
+    template: `
+    <div class="sidebar">
+        <div class = "row">
+            <div class = "col-4">
+            </div>
+            <div class = "col-6">
+                <br>
+                <img class="card-img-top cardImage"  alt="Card image cap" align="center">
+                <div class="card-body cardBodyInfo">
+                    <div class="grid cardInfoContainer cardTextInfo">
+                        <h6 class="card-title cardTextInfo" align="left">{{titl}}</h6>
+                        <div class="row">
+                            <div class="col-6">
+                                <h6 class="card-text cardTextInfo" align = "left">{{favt}}<br>{{desc}}<br>{{inst}}</h6>
+                            </div>
+                            <br>
+                            <div class="col-6">
+                                <button id="{{id}}" class="btn btn-primary">{{pric}}</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`,
+    props: {
+        //id
+        'id': String,
+        //titulo
+        'titl': String,
+        //favour type
+        'favt': String,
+        //short desc
+        'desc': String,
+        //instructions
+        'inst': String,
+        //price
+        'pric': String,
+    }
+})
--- views/ticket.component.js
+++ views/ticket.component.js
@@ -6,14 +6,14 @@ app.component('ticket', {
             <img class="card-img-top cardImage"  alt="Card image cap" align="center">
             <div class="card-body cardBodyInfo">
                 <div class="grid cardInfoContainer cardTextInfo">
-                    <h6 class="card-title cardTextInfo" align="left">{{d}}</h6>
+                    <h6 class="card-title cardTextInfo" align="left">{{titl}}</h6>
                     <div class="row">
                         <div class="col-6">
-                            <br>
-                            <h6 class="card-text cardTextInfo" align = "left">{{l}}<br>{{p}}</h6>
+                            <h6 class="card-text cardTextInfo" align = "left">{{favt}}<br>{{desc}}<br>{{inst}}</h6>
                         </div>
+                        <br>
                         <div class="col-6">
-                            <a href="{{h}}" class="btn btn-primary cardButton">Ver</a>
+                            <button id="{{id}}" class="btn btn-primary">{{pric}}</button>
                         </div>
                     </div>
                 </div>
@@ -21,9 +21,17 @@ app.component('ticket', {
         </div>
     </div>`,
     props: {
-        'd': String,
-        'l': String,
-        'p': String,
-        'h': String
+        //id
+        'id': String,
+        //titulo
+        'titl': String,
+        //favour type
+        'favt': String,
+        //short desc
+        'desc': String,
+        //instructions
+        'inst': String,
+        //price
+        'pric': String,
     }
 })
