35992797b42f101c4f6bf6db821129cad92ffa31
A01197185 <64107767+A01197185@users.noreply.github.com>
2022-06-14 12:55:53 -0500

    Cambio Posicion de botones
    Se cambio la posicion de los botones en el menu derecho

--- views/ticket.component.js
+++ views/ticket.component.js
@@ -11,13 +11,13 @@ app.component('ticket', {
                             <h6 class="card-text cardTextInfo">{{favt}}<br><br>{{desc}}<br><br>{{inst}}</h6>
                         </div>
                         <br>
-                        <div class="col-sm-10 col-xl-6"  v-if="but === 'assign'">
+                        <div class="col-sm-10 col-xl-10"  v-if="but === 'assign'">
                             <button class="btn btn-primary assign" @click="$emit('assign-ticket', id)">$ {{pric}}</button>
                         </div>
-                        <div class="col-sm-10 col-xl-6"  v-else-if="but === 'cancel'">
+                        <div class="col-sm-10 col-xl-10"  v-else-if="but === 'cancel'">
                             <button class="btn btn-primary assign" @click="$emit('cancel-ticket', id)">Cancel</button>
                         </div>
-                        <div class="col-sm-10 col-xl-6"  v-else-if="but === 'complete'">
+                        <div class="col-sm-10 col-xl-10"  v-else-if="but === 'complete'">
                             <button class="btn btn-primary assign" @click="$emit('complete-ticket', id)">Complete</button>
                         </div>
                     </div>
